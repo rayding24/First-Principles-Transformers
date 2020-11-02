ec9258ecfe77a3e90372bd3f213818ed907f7d2e
rayding <rayding2011@gmail.com>
2020-11-01 19:18:18 -0800

    proper pytorch attention, encoder

--- layers/attention.py
+++ layers/attention.py
@@ -2,6 +2,8 @@ import torch
 import math
 import torch.nn.functional as F
 from torch.autograd import Variable
+import torch.nn as nn
+from torch import einsum
 
 '''
 Ray Ding 
@@ -17,7 +19,24 @@ def attention(Q, K, V):
     attention = torch.matmul(softmax, V)
     return attention
 
-
+class SelfAttention(nn.Module):
+    def __init__(self, emb_dim, num_heads):
+        super().__init__()
+        # we want to output the same dim as embedding to enable residual connection
+        # init 3 matrices all the same
+        self.M_Q, self.M_V, self.M_K = \
+            [nn.Linear(emb_dim, emb_dim*num_heads, bias=False) for _ in range(3)]
+        
+        self.M_merge_heads = nn.Linear(emb_dim*num_heads, emb_dim, bias=False)
+        
+    def forward(self, x):
+        # get Q, K, V
+        Q = x@self.M_Q
+        K = x@self.M_K
+        V = x@self.M_V 
+        multi_att = attention(Q, K, V)
+        return self.M_merge_heads(multi_att)
+        
 
 if __name__ == '__main__':
     Q = Variable(torch.rand(20, 50))
