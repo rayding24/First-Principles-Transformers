3909d4bb9075463243ee4733906e39ee7ab7e1b5
rayding <rayding2011@gmail.com>
2020-11-03 19:17:24 -0800

    encoder

--- layers/attention.py
+++ layers/attention.py
@@ -37,20 +37,7 @@ class SelfAttentionWide(nn.Module):
         multi_att = attention(Q, K, V)
         return self.M_merge_heads(multi_att)
         
-class Encoder(nn.Module):
-    def __init__(self, emb_dim, num_heads):
-        super().__init__()
-        self.attention_layer = SelfAttentionWide(emb_dim, num_heads)
-        self.layernorm1 = nn.LayerNorm(emb_dim)
-        self.layernorm2 = nn.LayerNorm(emb_dim)
-        # this can be pretty much any mlp we want
-        self.mlp = nn.Sequential(nn.Linear(emb_dim, emb_dim), nn.ReLU())
-        
-    def forward(self, x):
-        x = x + self.attention_layer(x)
-        x = self.layernorm1(x)
-        x = x + self.mlp(x)
-        return self.layernorm2(x)
+
         
         
 if __name__ == '__main__':
